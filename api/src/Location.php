e3e13c705740f2cadae80a2b7c19a897b934e2d0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-09-26 19:25:18 +0300

    Add docblocks

--- api/src/Location.php
+++ api/src/Location.php
@@ -7,10 +7,23 @@ namespace bgphp15\nameless;
 */
 class Location
 {
+    /**
+     * @var float
+     */
     private $latitude;
 
+    /**
+     * @var float
+     */
     private $longitude;
 
+    /**
+     * @param  float $latitude
+     * @param  float $longitude
+     * @return self
+     * @throws InvalidArgumentException
+     * @throws OutOfBoundsException
+     */
     public static function fromCoordinates($latitude, $longitude)
     {
         if (!is_float($latitude) || !is_float($longitude)) {
@@ -28,6 +41,10 @@ class Location
         return new self($latitude, $longitude);
     }
         
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     */
     private function __construct($latitude, $longitude)
     {
         $this->latitude = $latitude;
