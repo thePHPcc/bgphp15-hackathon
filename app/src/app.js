6decf4dd8a24078c0ea5df42e01e0375335a48b7
Carl Vuorinen <carl.vuorinen@gmail.com>
2015-09-26 19:41:00 +0300

    Register container

--- app/src/app.js
+++ app/src/app.js
@@ -2,8 +2,11 @@
 
 angular
     .module('ContainerApp', [
-        'ngRoute'
+        'ngRoute',
+        'ngResource'
     ])
+    .constant('API_BASE_URL', 'http://37.60.230.241/bgphp15-hackathon/api')
+    
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
@@ -15,6 +18,30 @@ angular
             });
     })
     
-    .controller('MainController', function () {
+    .controller('MainController', function (Container) {
+        var vm = this;
+        vm.newContainer = null;
+        vm.error = null;
+        
+        vm.registerNew = function () {
+            vm.newContainer = null;
+            vm.error = null;
+            
+            var newContainer = new Container();
+
+            newContainer.$save().then(saveSuccess, saveError);
+            
+            function saveSuccess(data) {
+                vm.newContainer = data;
+            }
+            
+            function saveError(err) {
+                vm.error = err;
+            }
+        };
+    })
+    
+    .factory('Container', function ($resource, API_BASE_URL) {
+        return $resource(API_BASE_URL + "/containers/:id");
     });
 
