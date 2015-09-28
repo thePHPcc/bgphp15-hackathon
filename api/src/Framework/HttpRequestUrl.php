20722ab913c627de774e60dc330a8a6ca28cd7bb
Carl Vuorinen <carl.vuorinen@gmail.com>
2015-09-28 12:52:14 +0300

    Implement HttpRequestUrl::fromSuperglobals

--- api/src/Framework/HttpRequestUrl.php
+++ api/src/Framework/HttpRequestUrl.php
@@ -13,7 +13,12 @@ class HttpRequestUrl
 
     public static function fromSuperglobals()
     {
-        return new self('http://example.com/container');
+        $url = (isset($_SERVER['HTTPS']) ? 'https' : 'http')
+             . '://' . $_SERVER['HTTP_HOST']
+             . ($_SERVER['SERVER_PORT'] == '80' ? '' : ':' . $_SERVER['SERVER_PORT'])
+             . $_SERVER['REQUEST_URI'];
+             
+        return new self($url);
     }
 
     private function __construct($url)
